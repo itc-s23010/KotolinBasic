d3e3ea226574d58f046a405cc5835f8d77f4d78c
s23010 <s23010@std.it-college.ac.jp>
2024-05-08 17:10:24 +0900

    二章終わり

---
+++ src/main/kotlin/Chap05.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val a: Char = '蛇'
+    val b: Char = '猫'
+    val c: Char = '犬'
+    println("$a,$b,$c")
+    println("課題をそのままやっても面白くないので${a}と、${b}と、${c}をつかって文を作ります")
+    println("${a}と${b}を合体！出来上がったのは〜？${c}！！！")
+}
